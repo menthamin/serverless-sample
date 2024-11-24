fb8b0b555cb3bbb5dfeccf7fde6a872b39b97828
menthamin <menthamin@naver.com>
2024-11-24 22:34:21 +0900

    init

---
+++ src/functions/get-sum.ts
@@ -0,0 +1,13 @@
+import { add } from "../lib/math";
+
+export const handler = async (event: any) => {
+  const { num1, num2 } = event.queryStringParameters || {};
+  const result = add(Number(num1), Number(num2));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `The sum of ${num1} and ${num2} is ${result}.`,
+    }),
+  };
+};
