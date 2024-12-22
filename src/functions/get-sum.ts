7894b33ea4d6a4bb7a32ea3d9a940db0ed20eedc
menthamin <menthamin@naver.com>
2024-12-22 22:12:16 +0900

    update npm scripts

--- src/functions/get-sum.ts
+++ src/functions/get-sum.ts
@@ -3,7 +3,7 @@ import { add } from "../lib/math";
 export const handler = async (event: any) => {
   const { num1, num2 } = event.queryStringParameters || {};
   const result = add(Number(num1), Number(num2));
-  const ulid = require('ulid');
+  // const ulid = require('ulid');
 
   return {
     statusCode: 200,
